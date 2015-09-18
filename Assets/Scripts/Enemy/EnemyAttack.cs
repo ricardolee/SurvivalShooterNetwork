b82cd7e6727c34b6e29ac923f9c51ad6126a9556
Pengcheng Li <lpc1983@gmail.com>
2015-09-19 00:05:45 +0800

    Add audio for enemy attack

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -5,8 +5,7 @@ using System.Collections;
 using Toolkit;
 
 [RequireComponent(typeof(StateManager))]
-public class EnemyAttack : NetworkBehaviour
-{
+public class EnemyAttack : NetworkBehaviour {
 
     public enum AttackState
     {
@@ -16,31 +15,29 @@ public class EnemyAttack : NetworkBehaviour
     public float timeBetweenAttacks = 0.5f;
     public int attackDamage = 10;
 
-
+    
     GameObject targetPlayer;
 
     [StateMachineInject]
     StateMachine<AttackState> attachSM;
     Animator anim;
-
-    void Awake()
-    {
+    AudioSource audioSource;
+    
+    void Awake () {
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
         attachSM.Init(AttackState.Idle);
     }
+    
 
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player" && attachSM.CurrentState == AttackState.Idle)
-        {
+    void OnTriggerEnter (Collider other) {
+        if(other.gameObject.tag == "Player" && attachSM.CurrentState == AttackState.Idle) {
             targetPlayer = other.gameObject;
             attachSM.ChangeState(AttackState.Attack);
         }
     }
-
-    void OnTriggerExit(Collider other)
-    {
+    
+    void OnTriggerExit (Collider other) {
         if (other.gameObject == targetPlayer)
             attachSM.ChangeState(AttackState.Idle);
     }
@@ -53,31 +50,28 @@ public class EnemyAttack : NetworkBehaviour
 
 
     private Coroutine attackCoroutine;
-
+    
     [StateListener(state = AttackState.Attack, on = StateEvent.Enter)]
-    void AttackEnter()
-    {
+    void AttackEnter() {
         attackCoroutine = StartCoroutine(AttackEnumerator());
     }
 
     [StateListener(state = AttackState.Attack, on = StateEvent.Exit)]
-    void AttackExit()
-    {
+    void AttackExit() {
         if (attackCoroutine != null)
             StopCoroutine(attackCoroutine);
     }
 
 
-    IEnumerator AttackEnumerator()
-    {
+    IEnumerator AttackEnumerator() {
         EventDispatcher events = targetPlayer.GetComponent<EventDispatcher>();
         while (true && isActiveAndEnabled)
         {
-            Action playDeathCallback = () =>
-            {
-                attachSM.ChangeState(AttackState.Idle);
+            Action playDeathCallback = () => {
+                    attachSM.ChangeState(AttackState.Idle);
             };
-            events.Trigger(PlayerHealth.DamageEvent, attackDamage, playDeathCallback);
+            audioSource.Play();
+            events.Trigger(PlayerHealth.DamageEvent, attackDamage, playDeathCallback); 
             yield return new WaitForSeconds(timeBetweenAttacks);
         }
     }
